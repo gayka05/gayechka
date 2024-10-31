0b76b27c61352a211c218aa56c0cbc21633f7fb8
gayka05 <159255593+gayka05@users.noreply.github.com>
2024-11-01 00:22:55 +0300

    Update dishes.js

--- dishes.js
+++ dishes.js
@@ -1,66 +1,16 @@
+// dishes.js
+
 const dishes = [
-    {
-        keyword: "cheese_soup",
-        name: "Сырный суп",
-        price: 300,
-        category: "soup",
-        count: "300 мл",
-        image: "soups/sirnisup.jpg"
-    },
-    {
-        keyword: "tom_yum",
-        name: "Том Ям",
-        price: 250,
-        category: "soup",
-        count: "300 мл",
-        image: "soups/tomyam.jpg"
-    },
-    {
-        keyword: "chicken_soup",
-        name: "Куриный суп",
-        price: 200,
-        category: "soup",
-        count: "300 мл",
-        image: "soups/kurinisup.jpg"
-    },
-    {
-        keyword: "steak",
-        name: "Стейк",
-        price: 600,
-        category: "main dish",
-        count: "450 г",
-        image: "mains/steik.jpg"
-    },
-    {
-        keyword: "cabbage_rolls",
-        name: "Голубцы",
-        price: 550,
-        category: "main dish",
-        count: "300 г",
-        image: "mains/голубцы.jpg"
-    },
-    {
-        keyword: "shashlik",
-        name: "Шашлык",
-        price: 600,
-        category: "main dish",
-        count: "500 г",
-        image: "mains/shashlyk.jpg"
-    },
-    {
-        keyword: "orange_juice",
-        name: "Апельсиновый сок",
-        price: 150,
-        category: "drink",
-        count: "300 мл",
-        image: "drinks/апельсин.jpg"
-    },
-    {
-        keyword: "pomegranate_juice",
-        name: "Гранатовый сок",
-        price: 150,
-        category: "drink",
-        count: "300 мл",
-        image: "drinks/гранат.jpg"
-    }
+    // Soups
+    { name: "Сырный суп", price: 450, weight: "450 мл", image: "sirnisup.jpg", category: "soup", dataDish: "phobo" },
+    { name: "Том Ям", price: 450, weight: "450 мл", image: "tomyam.jpg", category: "soup", dataDish: "phoga" },
+    { name: "Куриный суп", price: 550, weight: "550 мл", image: "kurinisup.jpg", category: "soup", dataDish: "buntom" },
+    // Main dishes
+    { name: "Стейк", price: 550, weight: "550 мг", image: "steik.jpg", category: "main", dataDish: "buntron" },
+    { name: "Голубцы", price: 550, weight: "550 мг", image: "голубцы.jpg", category: "main", dataDish: "comdui" },
+    { name: "Шашлык", price: 550, weight: "550 мг", image: "шашлык.jpg", category: "main", dataDish: "comtom" },
+    // Drinks
+    { name: "Яблочный сок", price: 70, weight: "70 мл", image: "яблоко.jpg", category: "drink", dataDish: "tra" },
+    { name: "Апельсиновый сок", price: 180, weight: "180 мл", image: "апельсин.jpg", category: "drink", dataDish: "cafeden" },
+    { name: "Гранатовый сок", price: 180, weight: "180 мл", image: "гранат.jpg", category: "drink", dataDish: "cafesua" },
 ];
